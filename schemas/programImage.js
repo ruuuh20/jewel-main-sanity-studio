d8bfd5b24180dc6667b1c0517aa9214caa26f9dd
ruuuh20 <pkim2320@gmail.com>
2021-11-01 11:53:52 -0400

    init

---
+++ schemas/programImage.js
@@ -0,0 +1,20 @@
+export default {
+     
+       name: 'programImage',
+    title: 'Program Image',
+    type: 'image',
+    fields: [
+        {
+            name: 'caption',
+            title: 'Caption',
+            type: 'string',
+            options: {
+                isHighlight: true
+            }
+        }
+    ],
+    options: {
+        hotspot: true
+    }
+
+}
